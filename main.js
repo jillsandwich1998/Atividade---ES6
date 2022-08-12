bd93a93fbc065f2a8a4097637e5aa39812190825
Davi <davidias1000@gmail.com>
2022-08-12 14:25:38 -0300

    Atividade do segundo vídeo

--- main.js
+++ main.js
@@ -1,51 +1,37 @@
-class List{
-    constructor(){
-        this.data=[];
-    }
-    add(nome){
-        this.data.push(nome);
-        console.log(this.data);
-    }
+const arr = [1,2,3,4,6,8,9]
+
+
+// console.log(newArr)
+
+// const soma = arr.reduce(function(total, next){
+//     return total + next;
+// })
+
+// console.log(soma)
+
+// const filter = arr.filter(function(item){
+//     return item % 2 === 0; 
+// })
+
+// console.log(filter)
+
+// const find = arr.find(function(item){
+//     return item === 7;
+// })
+
+//console.log(find)
+
+//  const newArr = arr.map(item=>item * 2)
+
+//  console.log(newArr)
+
+//  const teste = () =>  {
+//     return {nome: 'Davi'}
+//  }
+//  console.log(teste())
+
+function soma (a=2, b=3){
+    return a+b;
 }
 
-class ToList extends List{
-    constructor() {
-        super();
-        this.usuario="Davi"
-    }
-    getUsuario(){
-        console.log(this.usuario)
-    }
-    
-    static soma (a,b){
-        return a+b
-    }
-}
-
-/* const tolist = new ToList()
-
-document.getElementById('novotodo').onclick = function(){
-    tolist.add('Davi');
-    tolist.getUsuario();
-} */
-
-//constantes
-
-//Var é universal
-//var nome = 'Davi'
-
-//Const não muda
-const nome = {primeiro_nome: "Davi"}
-
-nome.primeiro_nome = "João"
-
-console.log(nome);
-
-
-//Let pode apenas ser definido dentro do escopo de uma função
-function teste(x){
-    let y = 4;
-    if (x>2){y=6
-    console.log(x,y)}
-}
-console.log(teste(10))
+console.log(soma())
